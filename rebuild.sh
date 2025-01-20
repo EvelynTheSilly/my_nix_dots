034a7648f6604ee4f021394fd234f99936cb9ae1
vlad <vlad@nikulin.name>
2025-01-20 16:18:44 +0000

    Nix generation: 98 current  2025-01-20 15:54:02  25.05.20250112.ed4a395     6.6.71                          *

--- rebuild.sh
+++ rebuild.sh
@@ -4,8 +4,6 @@
 #command 1: sudo nixos-rebuild switch --flake .#desktop
 #command 2: home-manager switch --flake .#vlad@desktop
 
-git pull
-
 # Display help message
 show_help() {
   echo "Usage: $(basename "$0") [options]"

9d3110294ef5c9663d4912e03793620075766e50
vlad <vlad@nikulin.name>
2025-02-01 12:57:08 +0000

    Nix generation: 116 current  2025-02-01 12:56:56  25.05.20250121.9e4d519     6.12.10                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -63,6 +63,7 @@
     extraGroups = [ "wheel" "networkmanager" "docker"]; # Enable sudo and network manager for the user.
     packages = with pkgs; [ #Full user app list
       ollama-cuda
+      blueman
       bc
       transmission_4-qt
       tor-browser

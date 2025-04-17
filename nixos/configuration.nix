1540ffa3a7e70d339c6759c30830787074b2ac3c
vlad <vlad@nikulin.name>
2025-04-17 18:20:33 +0100

    Nix generation: 149 current  2025-04-17 18:20:30  25.05.20250412.2631b0b  6.12.23                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -116,12 +116,10 @@ in {
       tree
       (discord-canary.override {
         withVencord = true;
-        #withPipewire = true; # Explicitly enable PipeWire support
       })
       (
         discord-ptb.override {
-          withVencord = true;
-         # withPipewire = true; # Explicitly enable PipeWire support
+          #withVencord = true;
         }
       )
       kind

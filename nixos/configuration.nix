2d4a7ee0abae9820e83749243bfdb2708765f6e7
vlad <vlad@nikulin.name>
2025-04-22 18:40:03 +0100

    Nix generation: 26 current  2025-04-22 18:09:51  25.05.20250412.2631b0b     6.12.23                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -51,9 +51,13 @@ in {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
-  networking.nameservers = ["192.168.86.34" "1.1.1.1"];
-  
+  networking = {
+      networkmanager.enable = true; # Easiest to use and most distros use this by default.
+      nameservers = ["192.168.86.34" "1.1.1.1"];
+      dhcpcd.enable = false; # Optional: disable dhcpcd if you're using NetworkManager or systemd-networkd
+      useDHCP = false;
+  };
+
   # Set your time zone.
   time.timeZone = "Europe/London";
 

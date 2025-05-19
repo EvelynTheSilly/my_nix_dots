6cb6e66ca65661a43ee9057ca6ba374eadfd19d0
vlad <vlad@nikulin.name>
2025-05-19 15:01:31 +0100

    Nix generation: 156 current  2025-05-19 15:00:12  25.05.20250412.2631b0b  6.12.23                          *

---
+++ nixos/grub.nix
@@ -0,0 +1,16 @@
+{pkgs, lib, ...}: let
+    theme = pkgs.writeTextFile {
+        name = "theme.txt";
+        text = ''
+
+        '';
+    };
+in{
+    boot.loader.grub = {
+        enable = true;
+        devices = ["/dev/sda3"];
+        #theme = ./grub/catpuccin-mocha-grub-theme;
+        useOSProber = true;
+    };
+    timeout = 1;
+}

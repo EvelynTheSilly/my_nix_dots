3440d85b9b7b04e5769f548d63c29c490641e193
eve <vlad@nikulin.name>
2025-07-06 11:24:25 +0100

    Nix generation:

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
     catppuccin.url = "github:catppuccin/nix";
   };
 
+<<<<<<< HEAD
   outputs =
     {
       self,
@@ -82,6 +83,69 @@
           };
         };
 
+=======
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    hyprland,
+    catppuccin,
+    niri,
+    #hy3,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      desktop = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        # > Our main nixos configuration file <
+        modules = [
+          ./device_specific/desktop/configuration.nix
+          catppuccin.nixosModules.catppuccin
+          home-manager.nixosModules.home-manager
+          niri.nixosModules.niri
+        ];
+      };
+      laptop = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          ./device_specific/laptop/configuration.nix
+          catppuccin.nixosModules.catppuccin
+          home-manager.nixosModules.home-manager
+          niri.nixosModules.niri
+        ];
+      };
+    };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      # FIXME replace with your username@hostname
+      "vlad@desktop" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        # > Our main home-manager configuration file <
+        modules = [
+          #hyprland.homeManagerModules.default
+          ./device_specific/desktop/home.nix
+          catppuccin.homeManagerModules.catppuccin
+          niri.homeModules.config
+        ];
+      };
+      "vlad@laptop" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        # > Our main home-manager configuration file <
+        modules = [
+          #hyprland.homeManagerModules.default
+          ./device_specific/laptop/home.nix
+          catppuccin.homeManagerModules.catppuccin
+          niri.homeModules.config
+        ];
+>>>>>>> be468d8 (Nix generation:)
       };
     };
 }

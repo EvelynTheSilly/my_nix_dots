45a6a765f55a7afab119e87a71f30cb46d16eb5c
vlad <vlad@nikulin.name>
2025-07-19 23:00:02 +0100

    Nix generation:

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -70,7 +70,7 @@
   programs.kitty = {
     enable = true;
     settings = {
-      background_opacity = 0;
+      background_opacity = 0.2;
       cursor_shape = "beam";
       cursor_trail = 1;
       shell = "nu";

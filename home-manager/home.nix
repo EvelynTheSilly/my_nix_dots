c316e1ec16bf647fc3cca7e054b2ca9608acc684
vlad <vlad@nikulin.name>
2025-09-13 23:52:22 +0100

    Nix generation:

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,13 +7,8 @@
   pkgs,
   ...
 }: {
-  # You can import other home-manager modules here
   imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
     # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
     ./hypr.nix
     ./waybar.nix
     ./hyprutils.nix
@@ -27,17 +22,7 @@
   nixpkgs = {
     # You can add overlays here
     overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
-    # Configure your nixpkgs instance
     config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
@@ -51,17 +36,14 @@
     homeDirectory = "/home/vlad";
   };
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
   home.packages = with pkgs; [
     dunst
   ];
 
-  # Enable home-manager and git
   programs.home-manager.enable = true;
   programs.git = {
     enable = true;
-    userName = "eve";
+    userName = "Evelyn";
     userEmail = "vlad@nikulin.name";
     extraConfig = {
       pull.rebase = "true";
@@ -78,6 +60,7 @@
     };
   };
 
+  # Term
   programs.kitty = {
     enable = true;
     settings = {
@@ -90,15 +73,12 @@
     };
   };
 
+  # Shell 
   programs = {
     nushell = {
       enable = true;
-      # The config.nu can be anywhere you want if you like to edit your Nushell with Nu
       configFile.source = ./config.nu;
-      # for editing directly to config.nu
     };
-    #carapace.enable = true;
-    #carapace.enableNushellIntegration = true;
 
     starship = {
       enable = true;
@@ -111,7 +91,7 @@
       };
     };
   };
-  # Nicely reload system units when changing configs
+
   systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion

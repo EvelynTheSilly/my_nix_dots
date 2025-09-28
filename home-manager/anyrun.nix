d2fb27a0f703e724e3b152e200103df59476203f
vlad <vlad@nikulin.name>
2025-09-28 18:37:38 +0100

    Nix generation:

--- home-manager/anyrun.nix
+++ home-manager/anyrun.nix
@@ -10,6 +10,12 @@
       x = {
         fraction = 0.5;
       };
+      plugins = [
+        "${pkgs.anyrun}/lib/libapplications.so"
+        "${pkgs.anyrun}/lib/libwebsearch.so"
+        "${pkgs.anyrun}/lib/libsymbols.so"
+        "${pkgs.anyrun}/lib/librink.so"
+      ];
     };
   };
 }

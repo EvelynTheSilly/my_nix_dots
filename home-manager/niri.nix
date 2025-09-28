f141ed7593d360effcba6e4a91361dcac369a980
vlad <vlad@nikulin.name>
2025-09-28 18:47:00 +0100

    Nix generation:

--- home-manager/niri.nix
+++ home-manager/niri.nix
@@ -328,7 +328,7 @@
         Mod+T hotkey-overlay-title="Open a Terminal: kitty"          { spawn "kitty"; }
         Mod+B hotkey-overlay-title="Open a Browser: firefox"         { spawn "firefox"; }
         Mod+D hotkey-overlay-title="Run an Application: anyrun"      { spawn "anyrun"; }
-        Mod+D hotkey-overlay-title="App Switcher: anyrun"            { spawn "anyrun" "--plugins" "${pkgs.anyrun}/lib/libniri_focus.so"; }
+        Mod+Tab hotkey-overlay-title="App Switcher: anyrun"          { spawn "anyrun" "--plugins" "${pkgs.anyrun}/lib/libniri_focus.so"; }
         // Mod+S hotkey-overlay-title="Screenshot: hyprshot"         { spawn "hyprshot" "-m" "region"; }
 
         // Open/close the Overview: a zoomed-out view of workspaces and windows.

32cc8983ecf9068dad154745c9edeaaddfc0fc56
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com>
2025-10-22 20:35:54 +0100

    Home Generation: 2025-10-22 20:35 : id 334 -> /nix/store/nzs7va5smps2d3q2isa9l49sys25q891-home-manager-generation (current)

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -55,47 +55,7 @@
       };
     };
     style = ''
-        /* Base styling for the bar */
-        #waybar {
-          color: #ffffff;      /* White text color */
-          font-family: "Hack Nerd Font", "FiraCode Nerd Font", sans-serif;
-          font-size: 14px;
-          padding: 5px 10px;
-          border-bottom: 2px solid @blue; /* Accent color */
-        }
 
-        .modules-left {
-            padding-left: 7px;
-        }
-
-        .modules-right {
-          padding-right: 7px;
-        }
-
-        /* Styling for individual modules */
-        #waybar .module {
-          margin: 0 5px;
-          padding: 5px 8px;
-          border-radius: 5px;
-          background: @surface0; /* Slightly lighter background for modules */
-        }
-
-        /* Active or focused workspace styling */
-        #workspaces button.active {
-          background-color: @lavender; /* Green accent for active workspace */
-          color: @crust;
-          font-weight: bold;
-        }
-
-       /* Inactive workspaces */
-        #workspaces button.inactive {
-          color: #585b70; /* Dimmed color for inactive workspaces */
-        }
-
-        /* Separator styling */
-        #waybar .separator {
-         margin: 0 5px;
-        }
     '';
     
   };

c074fc1cc8979f3b7b2921c7af790c78086011b0
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com>
2025-10-22 19:04:56 +0100

    Home Generation: 2025-10-22 19:04 : id 326 -> /nix/store/z7jgw5vzbydmypjg5z01q53vcsqnl3nc-home-manager-generation (current)

--- home-manager/waybar_rewrite.nix
+++ home-manager/waybar_rewrite.nix
@@ -6,7 +6,53 @@
   programs.waybar = {
     enable = true;
     settings = {
-      
+       mainBar = {
+        layer = "top";
+        position = "top";
+        height = 50;
+        modules-left = [
+          "custom/power"
+          "hyprland/workspaces"
+          "niri/workspaces"
+        ];
+        modules-centre = ["karlstav/cava"];
+        modules-right = [
+          "tray"
+          "backlight"
+          "cpu"
+          "memory"
+          "battery"
+          "pulseaudio"
+          "clock"
+        ];
+        cpu = {
+          format = " {usage}%";
+          on-click = "kitty --class btop --hold -e btop";
+        };
+        memory = {
+          format = " {}%";
+          on-click = "kitty --class btop --hold -e btop";
+        };
+        pulseaudio = {
+          format = " {volume}%";
+        };
+        "custom/power" = {
+          format = "pwr";
+          tooltip = false;
+          on-click = "wlogout -b 2";
+          on-click-right = "wlogout --protocol layer-shell";
+          class = "power-button";
+        };
+        clock = {
+          tooltip-format = "{:%Y-%b-%d, %a}";
+        };
+        battery = {
+          format = "{capacity}  ";
+        };
+        backlight = {
+          format = "{percent}  ";
+        };
+      }; 
     };
   };
 }

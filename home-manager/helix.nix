c85a26c169a3cce89495b681580048e7221acfed
Evelyn <vlad@nikulin.name>
2025-12-06 01:30:26 +0000

    Nix generation: 12 2025-12-06 01:30:25 kernel 6.12.60

--- home-manager/helix.nix
+++ home-manager/helix.nix
@@ -6,11 +6,29 @@
   programs.helix = {
     enable = true;
     package = pkgs.evil-helix;
+    languages = {
+      language = [
+        {
+          name = "rust";
+          auto-format = true;
+          auto-pairs = {
+            "(" = ")";
+            "{" = "}";
+            "[" = "]";
+            "'" = "'";
+          };
+          language-server.rustanalyzer = {
+            command = "rust-analyzer";
+          };
+        }
+      ];
+    };
     settings = {
       theme = lib.mkForce "tokyonight";
       editor = {
         line-number = "relative";
         completion-timeout = 5;
+        clipboard-provider = "wayland";
         cursor-shape = {
           insert = "bar";
         };

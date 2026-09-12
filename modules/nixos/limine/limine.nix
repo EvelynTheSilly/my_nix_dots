3406e39396789dcaeea60532ecdf467fa7191173
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com.>
2026-09-12 19:54:33 +0100

    Nix generation: 102 2026-09-12 19:54:30 kernel 6.18.44

--- modules/nixos/limine/limine.nix
+++ modules/nixos/limine/limine.nix
@@ -12,6 +12,11 @@
       autoGenerateKeys = true;
       autoEnrollKeys.enable = true;
     };
+    extraConfig = ''
+        /Windows
+          protocol: efi
+          path: guid(1f54566a-264c-4d7a-a6a0-5fce8c0dc99c):/efi/microsoft/boot/bootmgfw.efi
+      '';
   };
   environment.systemPackages = with pkgs; [
     sbctl
